c57db3262d184fe3daa18ec93fa5554b8c486573
temitee5 <shalomtemitee@gmail.com>
2023-03-16 04:51:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - puts "\"Programming is like building a multilingual puzzle"
  * return: 0
 */
-int main(void)
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
