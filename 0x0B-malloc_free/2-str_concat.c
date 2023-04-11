34ac858c2dc186ef234348340170c7c248854c26
temitee5 <shalomtemitee@gmail.com>
2023-04-11 03:25:13 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-
 	int i, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
@@ -31,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
